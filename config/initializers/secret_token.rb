1e0a4838bc7efb04078fe21d0565c552e456109c
Donny Kurnia <donnykurnia@gmail.com>
2013-09-14 16:25:37 +0700

    rails 3.2
    Signed-off-by: Donny Kurnia <donnykurnia@gmail.com>

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DragDropExample::Application.config.secret_token = 'b36ccdf6f4293f546d0edbc8c5f03754c00f3af381a67d66af4bf06fd528f9a026c89b7d98aac84618fac84697cc58f155887eb1a935ab7fe80a062ba3c023b5'
